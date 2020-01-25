8e8e7f9606f555dec99f4e886c751626d8c3219e
yash3001 <43678393+yash3001@users.noreply.github.com>
2020-01-26 02:02:59 +0530

    Update mac_address.py

--- mac_address.py
+++ mac_address.py
@@ -1,22 +1,53 @@
 import subprocess
 import optparse
+import re
+
 
 def get_args():
 	parser = optparse.OptionParser()
 	parser.add_option('-i', '--interface', dest = 'interface', help = 'Interface to change its MAC address')
-	parser.add_option('-m', '--mac', dest = 'mac', help = 'New Mac Address')
+	parser.add_option('-m', '--mac', dest = 'new_mac', help = 'New Mac Address')
 	(options, arguments) = parser.parse_args()
 	if not options.interface:
 		parser.error('[-] Please specify an interface, use --help for more info.')
-	elif not options.mac:
+	elif not options.new_mac:
 		parser.error('[-] Please specify the new mac, use --help for more info.')
 	else:
 		return options
 
-def run_terminal(interface,mac):
-	print('[+] Changing Mac Address of interface ' + interface + ' to ' + mac)
-	subprocess.call(f"sudo ifconfig {interface} down && sudo ifconfig {interface} hw ether {mac} && sudo ifconfig {interface} up", shell = True)
+
+def run_terminal(interface,new_mac):
+	print('[+] Changing Mac Address of interface ' + interface + ' to ' + new_mac)
+	subprocess.call(["sudo", "ifconfig", interface, "down"])
+	subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
+	subprocess.call(["sudo", "ifconfig", interface, "up"])
+
+
+def return_mac(interface):
+	ifconfig_output = subprocess.check_output(['ifconfig',interface])
+	result = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', str(ifconfig_output))
+	return result.group(0)
+
+
+def check(interface,mac):
+	ifconfig_output = subprocess.check_output(['ifconfig',interface])		
+	result = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', str(ifconfig_output))
+	result = result.group(0)
+	if result:
+		if result == mac:
+			print('[+] Successfully changed.')
+
+		else:
+			print('[-] Couldn\'t change MAC Address.')
+	
+	else:
+		print('[-] Cannot find MAC Address. Aborting...')
+
 
 options = get_args()
-run_terminal(options.interface,options.mac)
-
+initial_mac = return_mac(options.interface)
+print("[+] Inital MAC address is ",initial_mac)
+run_terminal(options.interface,options.new_mac)
+check(options.interface,options.new_mac)
+final_mac = return_mac(options.interface)
+print("[+] Final MAC address is ",final_mac)
